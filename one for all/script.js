44ff8e6e963aba4cd0a8e414abdf55f734d465c8
Hamza-Sama <hamxu69@gmail.com>
2026-07-09 20:23:00 +0500

    push

--- one for all/script.js
+++ one for all/script.js
@@ -19,7 +19,34 @@ data.daily.time.forEach((el, index) => {
   console.log(
     data.daily.temperature_2m_min[index],
     data.daily.temperature_2m_max[index],
-    data.daily.weather_code[index],
+    getWeatherIcon(),
     
   );
 });
+function getWeatherIcon(weatherCode) {
+  if (weatherCode === 0) {
+    return "images/icon-sunny.webp";
+  }
+  if (weatherCode === 1 || weatherCode === 2) {
+    return "images/icon-partly-cloudy.webp";
+  }
+  if (weatherCode === 3) {
+    return "images/icon-overcast.webp";
+  }
+  if (weatherCode === 45 || weatherCode === 48) {
+    return "images/icon-fog.webp";
+  }
+  if ([51, 53, 55, 56, 57].includes(weatherCode)) {
+    return "images/icon-drizzle.webp";
+  }
+  if ([61, 63, 65, 66, 67, 80, 81, 82].includes(weatherCode)) {
+    return "images/icon-rain.webp";
+  }
+  if ([71, 73, 75, 77, 85, 86].includes(weatherCode)) {
+    return "images/icon-snow.webp";
+  }
+  if ([95, 96, 99].includes(weatherCode)) {
+    return "images/icon-storm.webp";
+  }
+  return "images/icon-search.svg";
+}
