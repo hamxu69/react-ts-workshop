ff4437a14704ebf55981ce2490303d6aaf3b4002
Hamza-Sama <184765708+hamxu69@users.noreply.github.com>
2026-08-01 22:26:43 +0500

    Update AlertButton.tsx

--- src/AlertButton.tsx
+++ src/AlertButton.tsx
@@ -1,6 +1,6 @@
 function AlertButton() {
     function AlertFunction() {
-        alert('This  button is working!')
+        alert('This is button is working!')
     }
     return (
         <button onClick={AlertFunction}>Click Me</button>
